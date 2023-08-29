4d5e1737cfc4f309f140dc50052b48d85f8dddc1
Malik <astemirmail@gmail.com>
2023-08-29 15:36:49 +0600

    Task21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координаты точки А");
+double x1 =  Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B");
+double  x2 = Convert.ToDouble(Console.ReadLine());
+double  y2 = Convert.ToDouble(Console.ReadLine());
+double  z2 = Convert.ToDouble(Console.ReadLine());
+
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+        
+
+Console.WriteLine(distance);
