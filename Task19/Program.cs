3341033f2a8a8d3ae0e6409fa8d828ddc2fedfc0
Malik <astemirmail@gmail.com>
2023-08-29 15:32:53 +0600

    Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Palindrome = Convert.ToInt32(Console.ReadLine());
+if (Palindrome/10000 == Palindrome%10 && Palindrome /1000%10 == Palindrome%100/10)
+Console.WriteLine ("Is Palindrome"); 
+else
+{
+    Console.WriteLine(" is not Palindrome");
+}
