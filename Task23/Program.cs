ae83f04ccf1549640830d101778517df195d2f73
Malik <astemirmail@gmail.com>
2023-08-29 15:38:24 +0600

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 0 ; i < 3 ; i++)
+{
+    result *= number;
+}
+Console.WriteLine("Число" + number +  "в кубе равно"  + result);
+
