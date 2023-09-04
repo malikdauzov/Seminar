68f9d8a9433bff2225983783a20f07d61d8268da
Malik <astemirmail@gmail.com>
2023-09-04 13:20:44 +0600

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Алгоритм нужно реализовать самостоятельно, встроенную функцию использовать нельзя.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Input a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input b");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+for (int i = 0; i < b; i++)
+{
+    result *= a;
+  
+        
+}
+   
+
+Console.WriteLine(result);
