3f01e36881403ac8e45c56bff2650f5bdf554d5e
Malik <astemirmail@gmail.com>
2023-09-18 22:05:35 +0600

    Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]:F1} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns]; 
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.NextDouble() * (max - min) + min;
+        }
+    }
+    return matrix;
+}
+
+// void FillArray(double[,] matr, double min, double max)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i, j] = new Random().NextDouble(min, max + 1);
+//         }
+//     }
+// }
+
+double[,] matrix = CreateMatrixRndDouble(3, 4, 1, 100);
+PrintArray(matrix);
+Console.WriteLine();
+
+
+
