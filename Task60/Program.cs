ee25e3f6df1fe0c4a37c089624bc58469f6f8439
Malik <astemirmail@gmail.com>
2023-09-22 10:19:50 +0600

    Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив,
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0) 27(0,0,1) 90(0,1,1)
+// 34(1,0,0) 41(1,1,0) 26(1,0,1) 55(1,1,1)
+
+
+int[,,] CreateArray3dRndInt(int rows, int columns, int depth, int min, int max)
+{
+    int[,,] matrix = new int[rows, columns, depth];
+    if (max > 99) Console.WriteLine("Укажите диапазон двузначных чисел.");
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = min++;
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write(matrix[i, j, k] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintMatrixToString(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i}, {j}, {k}) ");
+            }
+            //Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] array3d = CreateArray3dRndInt(2, 2, 2, 10, 99);
+PrintMatrix(array3d);
+Console.WriteLine();
+PrintMatrixToString(array3d); 
